a93b3aa2fa0c10eb4c71cf4025328f76458230e0
Blankman <60937724+jkblank@users.noreply.github.com>
2021-11-04 12:12:24 +0200

    Update Register.js
    logging

--- src/containers/register/Register.js
+++ src/containers/register/Register.js
@@ -31,7 +31,7 @@ class Register extends Component {
                 name: this.state.name,
                 email: this.state.email,
                 password: this.state.password
-            })
+            }).then(res => console.log(res))
         })
         .then(res => res.json())
         .then(user => {
