ab8131b6d4665d01eeabe1f74c8d3648813fb44f
PleaBanshee <llewellynant@gmail.com>
2021-11-10 14:10:14 +0200

    Added some styling to sidebar

--- src/components/page/Footer.js
+++ src/components/page/Footer.js
@@ -6,17 +6,12 @@ function Footer() {
       <ul className="links">
         <li>About</li>
         <li>Help</li>
-        <li>Press</li>
         <li>API</li>
-        <li>Jobs</li>
         <li>Privacy</li>
         <li>Terms</li>
-        <li>Locations</li>
-        <li>Top Accounts</li>
-        <li>Hashtags</li>
         <li>Language</li>
       </ul>
-      <div className="copyright">@ 2020 INSTAGRAM FROM FACEBOOK</div>
+      <div className="copyright">@ {`${new Date().getFullYear()}`} POSTALOT FROM THE BLANKBANSHEE</div>
     </div>
   );
 }
