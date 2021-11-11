527d8cac8bc73db23e63f6aede63c46733e32cf1
PleaBanshee <llewellynant@gmail.com>
2021-11-11 13:46:47 +0200

    Made pages more mobile responsive

--- src/components/page/Card.js
+++ src/components/page/Card.js
@@ -2,15 +2,11 @@ import "../../styles/card.scss";
 import Profile from "./Profile";
 import { ReactComponent as CardButton } from "../../images/cardButton.svg";
 import CardMenu from "./CardMenu";
-import Comment from "./Comment";
 
 function Card(props) {
   const {
     storyBorder,
     image,
-    comments,
-    likedByText,
-    likedByNumber,
     hours,
   } = props;
 
@@ -24,27 +20,8 @@ function Card(props) {
       <CardMenu />
       <div className="likedBy">
         <Profile iconSize="small" hideAccountName={true} />
-        <span>
-          Liked by <strong>{likedByText}</strong> and{" "}
-          <strong>{likedByNumber} others</strong>
-        </span>
-      </div>
-      <div className="comments">
-        {comments.map((comment) => {
-          return (
-            <Comment
-              key={comment.id}
-              accountName={comment.user}
-              comment={comment.text}
-            />
-          );
-        })}
       </div>
       <div className="timePosted">{hours} HOURS AGO</div>
-      <div className="addComment">
-        <div className="commentText">Add a comment...</div>
-        <div className="postText">Post</div>
-      </div>
     </div>
   );
 }
